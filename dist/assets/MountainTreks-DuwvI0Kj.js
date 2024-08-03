b63952ef7fc559de1115a55d77863ff1b9be9880
suman teja <sumanteja555@gmail.com>
2024-08-03 08:54:45 +0530

    previous commit with build

--- dist/assets/MountainTreks-DuwvI0Kj.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-C8h1KHN1.js";import t from"./mountainTreks-B1roiyuD.js";import{G as o}from"./GridLayout-BFiCc9pG.js";function n(){return r.jsx(o,{gridItems:t[0]})}export{n as default};
---
+++ dist/assets/WeekendGateway-Bz4Pqp64.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DDrWfsxJ.js";import{G as t}from"./GridLayout-Dxa-TgcQ.js";import r from"./weekendGateway-BVhIQ5tE.js";function s(){return e.jsx(t,{gridItems:r[0]})}export{s as default};
